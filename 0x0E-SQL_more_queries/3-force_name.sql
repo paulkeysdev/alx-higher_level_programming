877eabe6759398d5304aa2cef45497b077e60923
Paul Wainaina <wainainapaul6880@gmail.com>
2023-12-26 02:54:31 -0800

    Added task 4 updated

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,3 @@
 -- a script that creates the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXIST force_name (id INT , name VARCHAR(256) NOT NULL));
-
+CREATE TABLE IF NOT EXISTS force_name (id INT , name VARCHAR(256) NOT NULL));
 
